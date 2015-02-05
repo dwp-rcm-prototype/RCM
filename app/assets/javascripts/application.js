a44ebe490167e4a665ec60c7be73de2730f756b2
Gup <gurpreetbadyal@hotmail.com>
2015-02-05 12:38:28 +0000

    first 3 pages demo in washington

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,40 @@
+(function() {
+
+  var hideShow = function () {
+    var inputs = document.querySelectorAll('.hide-show');
+
+    for(var i=0; i < inputs.length; i++) {
+      if (inputs[i].checked) {
+        if(inputs[i].value === 'No-dob') {
+            document.querySelector('#dob-reveal').className = 'panel-indent js-hidden';
+            document.querySelector('#approx-age').className = 'panel-indent';
+          }
+          else if(inputs[i].value === 'Yes-dob') {
+              document.querySelector('#approx-age').className = 'panel-indent js-hidden';
+              document.querySelector('#dob-reveal').className = 'panel-indent';
+            }
+        else if(inputs[i].value === 'No') {
+            inputs[i].parentElement.parentElement.nextElementSibling.className = 'panel-indent js-hidden';
+          }
+        else {
+          inputs[i].parentElement.parentElement.nextElementSibling.className = 'panel-indent';
+        }
+      } else {
+        inputs[i].parentElement.parentElement.nextElementSibling.className = 'panel-indent js-hidden';
+      }
+    };
+  };
+
+  bindEvents = function () {
+    var inputs = document.querySelectorAll('.hide-show');
+
+    for (var i = 0; i < inputs.length; i++) {
+      inputs[i].addEventListener("click", hideShow, true );
+    };
+  };
+
+  return {
+    bindEvents : bindEvents()
+  };
+
+})();
